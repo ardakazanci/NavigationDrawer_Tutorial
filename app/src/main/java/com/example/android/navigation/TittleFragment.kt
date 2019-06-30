97aadf878cc7285d69d1719db35360fe1f26bfcc
ardakazanci <ardakazanci@anadolu.edu.tr>
2019-06-30 19:24:03 +0300

    Navigation System  Exercise - Commit 3

--- app/src/main/java/com/example/android/navigation/TittleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TittleFragment.kt
@@ -2,23 +2,16 @@ package com.example.android.navigation
 
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- *
- */
 class TittleFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -32,9 +25,41 @@ class TittleFragment : Fragment() {
         )
 
 
+        /*
+
+                // Birinci yol
+             binding.playButton.setOnClickListener {
+
+             Navigation.findNavController(it).navigate(R.id.action_tittleFragment_to_gameFragment)
+
+
+
+         }*/
+
+        // İkinci Yol
+        binding.playButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_tittleFragment_to_gameFragment))
+
+
+        setHasOptionsMenu(true)
+
+
         return binding.root
 
     }
 
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.overflow_menu, menu)
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return NavigationUI.onNavDestinationSelected(item!!,view!!.findNavController()) || super.onOptionsItemSelected(item)
+
+
+
+
+    }
 
 }
